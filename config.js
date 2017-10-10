1025c45e04d3968c5b710b0609dadc5df57c3214
ulfdahlstrand <ulf.d.dahlstrand@gmail.com>
2017-10-10 21:07:17 +0200

    added con for connecting to communicator

--- config.js
+++ config.js
@@ -12,5 +12,17 @@ module.exports = {
 	    id: process.env.GOOGLE_CLIENT_ID || '13824586724-fhh3e76je16ce10gvk9ucb66eu2oig8p.apps.googleusercontent.com',
 	    secret: process.env.GOOGLE_CLIENT_SECRET || 'lJlqp7HNvrSMJJbkzGLk43Aa',
 	  }
+	},
+	communicator:{
+		path: process.env.COMMUNICATOR_PATH || "",
+		token: process.env.COMMUNICATOR_TOKEN || ""
+	},
+	service: {
+		name: "uptiverse-authentication",
+		host: process.env.SERVICE_URL || ""
+	},
+	pulse: {
+		shouldRegister: true,
+		path:"/api/pulse"
 	}
 };
